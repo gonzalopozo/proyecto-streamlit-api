e74664eb1558525372ff8aa6d13e646e4cbd4526
gonzalopozo <gonzalopozo2005@gmail.com>
2025-04-10 10:11:35 +0200

    chore: Configuración inicial del proyecto

---
+++ app.py
@@ -0,0 +1,11 @@
+from config.page_config import setup_page
+setup_page()
+
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.write('Hello, world')
+
+if __name__ == '__main__':
+    main()
---
+++ config/page_config.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def setup_page():
+    st.set_page_config(
+        page_title='MEALSA DASHBOARD',
+        page_icon='./assets/favicon/favicon.png',
+        layout="wide"
+    )
